33151a91fdc64930783f912154df03edcd0d8e2a
izme <91523388+phamtranlinhchi@users.noreply.github.com>
2022-03-07 16:35:34 +0700

    update

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -3,17 +3,15 @@ const express = require("express");
 const userController = require("../controllers/userController");
 const userRouter = express.Router();
 
-userRouter.use(express.json());
+userRouter
+    .route("/")
+    .get(userController.getAllUsers)
+    .post(userController.createUser);
 
 userRouter
-	.route("/")
-	.get(userController.getAllUsers)
-	.post(userController.createUser);
-
-userRouter
-	.route("/:id")
-	.get(userController.getUser)
-	.put(userController.updateUser)
-	.delete(userController.deleteUser);
+    .route("/:id")
+    .get(userController.getUser)
+    .put(userController.updateUser)
+    .delete(userController.deleteUser);
 
 module.exports = userRouter;
